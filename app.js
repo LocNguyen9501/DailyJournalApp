5a791bae3f1419529cf2765f3317635a6a997264
Nguyenxloc <nguyenxloc@Nguyenxlocs-MacBook-Pro.local>
2020-07-07 17:19:56 +0700

    Completed register login functions

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs")
 const _ = require("lodash")
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,17 +19,36 @@ mongoose.connect("mongodb://localhost:27017/blogDB",{
   useUnifiedTopology: true
 })
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(session({
+  secret:process.env.SECRET,
+  resave:false,
+  saveUninitialized: true,
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 const postSchema = {
   title: String,
   content: String,
 }
 
-const Post = mongoose.model("Posts",postSchema)
+const Post = mongoose.model("Post",postSchema);
 
-app.set('view engine', 'ejs');
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  posts: [postSchema]
+})
+userSchema.plugin(passportLocalMongoose);
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+const User = mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
   res.render("about",{aboutInfo:aboutContent})
@@ -34,49 +56,84 @@ app.get("/", function(req,res){
 
 app.get("/home",function(req,res){
 
-  Post.find({},function(err,foundPost){
-    if(err){
-      console.log(err);
-    }else{
-    res.render("home",{postList:foundPost, homeContent: homeStartingContent})
-    }
-  })
+  if(req.isAuthenticated()){
+    User.findOne({_id:req.user.id, posts:{$exists:true, $ne:[]}},function(err,foundUser){
+      if(err){
+        console.log(err);
+      }else{
+        if(foundUser){
+          res.render("home",{homeContent:homeStartingContent,posts:foundUser.posts})
+        }else{
+          res.render("home",{homeContent:homeStartingContent})
+        }
+      }
+    })
+  }else{
+    res.redirect("/login")
+  }
 })
 
 app.get("/contact",function(req,res){
   res.render("contact", {contactInfo: contactContent})
 })
 
-app.get("/posts/:postID",function(req,res){
-  const postID = req.params.postID;
-
-  Post.find({_id: postID},function(err,foundPost){
-    if(!err){
-      res.render("post",{title:foundPost[0].title,content:foundPost[0].content})
-    }
-    else{
+app.get("/posts/:postId",function(req,res){
+  const postId = req.params.postId;
+  User.findOne({_id: req.user.id},function(err,foundUser){
+    if(err){
       console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.posts.filter(post=>{
+          if(post.id === postId){
+            console.log("This is the post "+post);
+            res.render("post", {foundPost: post});
+          }
+        })
+      }
     }
   })
 })
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 //////////////////////// Compose route /////////////////////
 app.route("/compose")
 
 .get(function(req,res){
-  res.render("compose")
+  if(req.isAuthenticated()){
+    res.render("compose")
+  }else{
+    res.redirect("/login")
+  }
 })
 
 .post(function(req,res){
-  const newItem = new Post({
-    title: req.body.postTitle,
-    content: req.body.postContent
+
+  const newPost = new Post({
+    title: req.body.title,
+    content: req.body.content,
   })
-  newItem.save(function(err){
-    if (!err){
-    res.redirect("/home");
-  }
-})
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.posts.push(newPost);
+        foundUser.save(function(err){
+          if(err){
+            console.log(err);
+          }
+        });
+
+        res.redirect("/home");
+      }
+    }
+  })
 });
 
 //////////////////////// Login route ////////////////////////
@@ -86,7 +143,23 @@ app.route("/login")
   res.render("login")
 })
 
-.post();
+.post(function(req,res){
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+
+  req.login(user,function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/home");
+      })
+    }
+  })
+});
 
 //////////////////////// Register route /////////////////////
 app.route("/register")
@@ -96,12 +169,14 @@ app.route("/register")
 })
 
 .post(function(req,res){
-
+  User.register({username:req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+    }else{
+        res.redirect("/login")
+    }
+  })
 });
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
