4ed69de950823fef019a5b95ca000dbdd61d05e7
Nguyenxloc <nguyenxloc@Nguyenxlocs-MacBook-Pro.local>
2020-07-10 16:55:44 +0700

    Update procfile, port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/blogDB",{
+mongoose.connect("mongodb+srv://nguyenxloc:loclocloc@cluster0.njxjk.mongodb.net/blogDB",{
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -202,6 +202,8 @@ app.route("/register")
   })
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
